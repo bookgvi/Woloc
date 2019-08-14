bee3e8acbdaa6ed5fda5013ae272afa3c3e13f68
Andrew Alexandroff <shurochkin@gmail.com>
2019-08-14 11:55:40 +0300

    lint

--- src/components/Calendar/Sheet/index.vue
+++ src/components/Calendar/Sheet/index.vue
@@ -1,24 +1,24 @@
 <template lang="pug">
   .sheet
     q-toolbar
-      q-btn(stretch='' flat='' label='Prev' @click='calendarPrev')
-        q-separator(vertical='')
-      q-btn(stretch='' flat='' label='Next' @click='calendarNext')
+      q-btn(stretch flat label="Prev" @click="calendarPrev")
+        q-separator(vertical)
+      q-btn(stretch flat label="Next" @click="calendarNext")
         q-space
         q-separator
-    q-calendar(ref='calendar'
-      hour24-format=true
-      now
-      max-days="7"
-      :interval-start="8"
-      :interval-count="16"
+    q-calendar(
+      ref="calendar"
       :resources="resources"
-      v-model='selectedDate'
-      view="scheduler"
-      locale='ru-ru'
+      v-model="selectedDate"
+      view="week"
+      locale="ru-ru"
       short-weekday-label
-      body=''
-      )
+      body
+      animated
+      hour24-format
+      transition-prev="slide-right"
+      transition-next="slide-left"
+    )
 </template>
 
 <script>
@@ -28,24 +28,7 @@ export default {
   data () {
     return {
       selectedDate: '',
-      resources: [
-        { label: '08:00 - 09:00' },
-        { label: '09:00 - 10:00' },
-        { label: '10:00 - 11:00' },
-        { label: '11:00 - 12:00' },
-        { label: '12:00 - 13:00' },
-        { label: '13:00 - 14:00' },
-        { label: '14:00 - 15:00' },
-        { label: '15:00 - 16:00' },
-        { label: '16:00 - 17:00' },
-        { label: '17:00 - 18:00' },
-        { label: '18:00 - 19:00' },
-        { label: '19:00 - 20:00' },
-        { label: '20:00 - 21:00' },
-        { label: '21:00 - 22:00' },
-        { label: '22:00 - 23:00' },
-        { label: '23:00 - 00:00' }
-      ]
+      resources: []
     }
   },
   methods: {
