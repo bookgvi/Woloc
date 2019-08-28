1cea7b5e1b1ef63e8077ef5be0f57be11242e5d1
MSST <leo2120@yandex.ru>
2019-08-28 21:22:58 +0300

    Users page structure changes, some bugs fixed

---
+++ src/components/Users/Index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  .users
+    UsersMenu
+    q-separator.q-mt-xs.q-px-none.q-my-md
+    UsersTable
+</template>
+
+<script>
+import UsersMenu from './Menu/Index'
+import UsersTable from './Table/Index'
+
+export default {
+  name: 'UsersIndex',
+  components: { UsersMenu, UsersTable },
+}
+</script>
+
+<style scoped>
+
+</style>
