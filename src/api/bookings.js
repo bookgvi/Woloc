320b6bf1aa4e3b9597c974d8236a7c34bbdbfd65
HiggsBison <1444666@gmail.com>
2019-08-30 19:41:19 +0300

    UGOLOC-694 change table template to use custom table row, add missing styles

--- src/api/bookings.js
+++ src/api/bookings.js
@@ -2,8 +2,14 @@ import api from './instance'
 
 export default {
   getAll: async () => {
+    const params = {
+      studio: '100',
+      dateFrom: '20190501',
+      dateTo: '20200101'
+    }
+
     try {
-      const r = await api.get(`bookings`)
+      const r = await api.get('bookings', { params })
       return r.data
     } catch (e) {
       console.warn('catch :: bookings :: getAll', e)
--- src/components/Bookings/index.vue
+++ src/components/Bookings/index.vue
@@ -22,21 +22,25 @@
         row-key="id"
         hide-bottom
       )
-        template(v-slot:body-cell-room="props")
-          q-td.room(:props="props")
-            q-chip(dense square :color="props.value.color" :title="props.value.name") {{props.value.name}}
-        template(v-slot:body-cell-eventType="props")
-          q-td(:props="props")
-            q-icon(size="md" :name='getEventIcon(props.value)')
-        template(v-slot:body-cell-isPaid="props")
-          q-td(:props="props")
-            q-icon(size="md" name="check" color="green" v-if="props.value")
-        template(v-slot:body-cell-comment="props")
-          q-td.comment(:props="props")
-            span(:title="props.value") {{props.value}}
-        template(v-slot:body-cell-actions="props")
-          q-td.flex.items-center(:props="props")
-            i(class="fas fa-ellipsis-v")
+        template(v-slot:body="props")
+          q-tr(:props="props" :class="props.row.removed && 'removed'")
+            q-td(
+              v-for="{name, value} of props.cols"
+              :key="name"
+              :props="props"
+              :class="name"
+            )
+              template(v-if="name === 'room'")
+                q-chip(dense square :color="value.color" :title="value.name") {{value.name}}
+              template(v-else-if="name === 'eventType'")
+                q-icon(size="sm" :name='getEventIcon(value)')
+              template(v-else-if="name === 'isPaid'")
+                q-icon(size="sm" name="check" color="green" v-if="value")
+              template(v-else-if="name === 'comment'")
+                span(:title="value") {{value}}
+              template(v-else-if="name === 'actions'")
+                i(class="fas fa-ellipsis-v")
+              template(v-else) {{ value }}
 </template>
 
 <script>
@@ -48,8 +52,8 @@ export default {
   name: 'Bookings',
   methods: {
     async getBookings () {
-      // await this.$app.bookings.getAll()
-      // this.bookings = this.$app.bookings.list
+      await this.$app.bookings.getAll()
+      this.bookings = this.$app.bookings.list
       this.bookings = bookings
     },
     getEventIcon (eventType) {
@@ -69,4 +73,25 @@ export default {
 </script>
 
 <style lang="stylus">
+  thead tr:first-child th
+    opacity 1
+    background-color $grey-12
+    text-transform uppercase
+    text-align left
+  tbody
+    tr.removed
+      opacity: .2
+    .room
+      max-width 80px
+      .q-chip
+        width 100%
+        div
+          width 100%
+          overflow hidden
+          text-overflow ellipsis
+
+    .comment
+      max-width 100px
+      overflow hidden
+      text-overflow ellipsis
 </style>
