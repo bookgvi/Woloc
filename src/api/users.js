02b4d4ef2cdc6b576184f4467ba6a7f586114514
zarvat <zarvat@localhost.localdomain>
2019-09-08 23:54:46 +0700

    fix typo

--- src/api/users.js
+++ src/api/users.js
@@ -4,7 +4,7 @@ export default {
 
   getSearchedUsers: async (payload) => {
     try {
-      const result = await api.post('/customers', payload)
+      const result = await api.post('customers', payload)
       return result
     } catch (e) {
       console.warn('catch :: users :: getSearchedUsers', e)
