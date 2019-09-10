d2ac07b6eea2f3e8c2250ad4c3d780da9895873f
Andrew Alexandroff <shurochkin@gmail.com>
2019-09-10 11:00:13 +0300

    wip

---
+++ src/components/Customers/index.vue
@@ -0,0 +1,22 @@
+<script>
+import DataTable from '../DataTable/index'
+import columns from './Table/columns'
+import Filters from '../Filters/index'
+
+export default {
+  name: 'CustomersPage',
+  components: { Filters, DataTable },
+  data () {
+    return {
+      columns
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+  q-page
+    .wrapper
+      Filters
+
+</template>
---
+++ src/pages/Customers.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  q-page
+    Customers
+</template>
+
+<script>
+import Customers from '../components/Customers'
+
+export default {
+  name: 'bookings-page',
+  components: { Customers },
+}
+</script>
