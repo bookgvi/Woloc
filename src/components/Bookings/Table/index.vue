d4314a200e8cae5ca5708adab909baa052a8f851
Andrew Alexandroff <shurochkin@gmail.com>
2019-09-10 12:26:53 +0300

    wip

--- src/components/Bookings/Table/index.vue
+++ src/components/Bookings/Table/index.vue
@@ -1,12 +1,15 @@
 <template lang="pug">
-
   DataTable(
     title="Бронирования"
+    :getDialogTitle="(row) => `Бронь ${row.id}`"
     :controller="$app.bookings"
     :columns="columns"
     :details="details"
   )
-    template(v-slot:row-controls="props")
+    template(#row-dialog="props")
+      BookingsDialog(v-bind="props")
+
+    template(#row-controls="props")
       q-btn(flat round icon="comment" title="Открыть чат")
       q-btn(flat round icon="delete" title="Удалить")
       q-btn(flat round icon="edit" @click="props.toggleDialogRow(props.row.id)" title="Редактировать")
@@ -18,6 +21,7 @@
 import columns from './columns'
 import details from './details'
 import DataTable from '../../DataTable'
+import BookingsDialog from './BookingsDialog'
 
 export default {
   name: 'BookingsTable',
