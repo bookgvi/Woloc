c9f7f31e55f065d822e0aca24e24b338808a4cad
bookgvi <C:\Users\bookgvi\AppData\Roaming\The Bat!>
2019-09-13 11:47:22 +0300

    UGOLOC-732: Верстка данных локации

---
+++ src/components/Settings/index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .wrapper
+    .row.items-center.q-py-sm.q-gutter-x-sm
+      q-input(label="test" outlined dense)
+    .row.justify-center
+      .col-6
+        datas
+</template>
+
+<script>
+import datas from './datas'
+export default {
+  components: { datas }
+}
+</script>
