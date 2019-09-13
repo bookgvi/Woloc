7a5c86fddc4ecdc4fd0fe615cfb7d641bfd80cb9
bookgvi <C:\Users\bookgvi\AppData\Roaming\The Bat!>
2019-09-13 12:39:21 +0300

    UGOLOC-732: Верстка блока фильтра

--- src/components/Settings/index.vue
+++ src/components/Settings/index.vue
@@ -1,7 +1,11 @@
 <template lang="pug">
   .wrapper
-    .row.items-center.q-py-sm.q-gutter-x-sm
-      q-input(label="test" outlined dense)
+    q-separator
+    .row.items-between.q-py-sm.q-gutter-x-sm
+      q-btn.col-3.q-btn--no-uppercase.q-px-none(label="Kap's Studios м. Бауманская" outlined dense flat)
+      q-space
+      q-btn.col-2.q-btn--no-uppercase(label="Добавить локацию" dense color="primary")
+    q-separator
     .row.justify-center
       .col-6
         datas
