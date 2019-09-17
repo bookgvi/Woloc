5b524022a76d8c3941c4804ae1303323a1d94ef8
bookgvi <C:\Users\bookgvi\AppData\Roaming\The Bat!>
2019-09-17 18:10:06 +0300

    UGOLOC-741: К странице подключены фильтры

--- src/components/Finances/index.vue
+++ src/components/Finances/index.vue
@@ -1,17 +1,26 @@
 <template lang="pug">
-  .refunds
-    Menu
+  .finances
+    filters-list(
+      name="finance"
+      v-slot:default="props"
+    )
+      studio-filter(v-bind="props")
+      rooms-filter(v-bind="props")
     FinancesTable
 </template>
 
 <script>
+import StudioFilter from 'components/Filters/StudioFilter'
+import FiltersList from 'components/Filters/FiltersList'
+import RoomsFilter from 'components/Filters/RoomsFilter'
 import FinancesTable from './Table/index'
-import Menu from '../Menu'
 export default {
   name: 'RefundsIndex',
   components: {
-    Menu,
-    FinancesTable,
+    StudioFilter,
+    RoomsFilter,
+    FiltersList,
+    FinancesTable
   }
 }
 </script>
