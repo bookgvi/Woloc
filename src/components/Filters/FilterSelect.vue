f4e0c3b23f40dfaa87fa6a817efbd74b2e59b364
HiggsBison <1444666@gmail.com>
2019-09-18 00:40:14 +0300

    UGOLOC-736 apply filter by buttons

--- src/components/Filters/FilterSelect.vue
+++ src/components/Filters/FilterSelect.vue
@@ -6,19 +6,42 @@
     :label="title"
     :disabled="disabled"
   )
-    q-popup-proxy
+    q-popup-proxy(ref="QPopupProxy")
       q-option-group.text-body2.q-pa-md(
         color="black"
         :type="type"
         :options="listOptions"
-        :value="value"
-        @input="$emit('change', $event)"
+        v-model="currentValue"
       )
+      q-btn-group.q-pa-md(outline)
+        q-btn.q-mr-md(
+          label="Отменить"
+          no-caps
+          :style="{ border: '#f0f0f0 solid 1px' }"
+          @click="onReset"
+        )
+        q-btn(
+          label="Применить"
+          color="primary"
+          no-caps
+          @click="onApply"
+        )
 </template>
 
 <script>
 export default {
   name: 'filter-select',
+  data: () => ({
+    currentValue: [],
+  }),
+  created () {
+    this.currentValue = this.value
+  },
+  watch: {
+    value (value) {
+      this.currentValue = value
+    }
+  },
   props: {
     title: String,
     models: Array,
@@ -43,6 +66,22 @@ export default {
     disabled () {
       return !this.listOptions.length
     }
+  },
+  methods: {
+    hidePopup () {
+      this.$refs.QPopupProxy.hide()
+    },
+    onApply () {
+      if (this.currentValue !== this.value) {
+        this.$emit('change', this.currentValue)
+      }
+
+      this.hidePopup()
+    },
+    onReset () {
+      this.currentValue = this.value
+      this.hidePopup()
+    },
   }
 }
 </script>
