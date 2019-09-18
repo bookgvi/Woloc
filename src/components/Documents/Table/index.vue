cd17ff4a469dbc132105653ff49b5055b4d47520
Vitaliy Guselnikov <vitaliy.guselnikov@ucann.ru>
2019-09-18 18:52:04 +0300

    UGOLOC-744: Изменен функционал для скачивания файла, добавлена кнопка-фильтр 'Дата и время'

--- src/components/Documents/Table/index.vue
+++ src/components/Documents/Table/index.vue
@@ -1,34 +1,30 @@
 <template lang="pug">
-  DataTable(
-    title="Документы"
-    :columns="columns"
-    :details="details"
-    :getDialogTitle="() => 'Документы'"
-    :loadData="$app.documents.getAll"
-  )
-    template(#row-controls="props")
-      q-btn(flat icon-right="get_app" title="Скачать" @click="handle(props.row.link)")
+  .table_document
+    DataTable(
+      title="Документы"
+      :columns="columns"
+      :details="details"
+      :getDialogTitle="() => 'Документы'"
+      :loadData="$app.documents.getAll"
+    )
+      template(#row-controls="props")
+        a(:href="props.row.link" title="Скачать")
+          q-icon(name="get_app" style="font-size: 2em")
 </template>
 
 <script>
 import columns from './columns'
 import details from './details'
 import DataTable from 'components/DataTable'
-import axios from 'axios'
 export default {
   name: 'DocumentsTable',
   components: { DataTable },
   data: () => ({
     columns,
-    details
+    details,
+    link: '',
+    alert: false
   }),
-  methods: {
-    async handle (url) {
-      try {
-        await axios(url)
-      } catch (err) {}
-    }
-  }
 }
 </script>
 
--- src/components/Documents/index.vue
+++ src/components/Documents/index.vue
@@ -1,14 +1,29 @@
 <template lang="pug">
   .documents
+    filters-list(
+      name="documents"
+      v-slot:default="props"
+    )
+      q-btn.q-py-none.q-px-sm(
+        outline
+        dense
+        no-caps
+        label="Дата и время"
+      )
     DocumentsTable
 </template>
 
 <script>
 import DocumentsTable from './Table/index.vue'
-
+import StudioFilter from '../Filters/StudioFilter'
+import RoomsFilter from '../Filters/RoomsFilter'
+import FiltersList from '../Filters/FiltersList'
 export default {
   components: {
-    DocumentsTable
+    DocumentsTable,
+    StudioFilter,
+    RoomsFilter,
+    FiltersList
   }
 }
 </script>
