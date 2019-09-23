cd8201998c9d65f3d834ef38d14078c8f93e66cd
Vitaliy Guselnikov <vitaliy.guselnikov@ucann.ru>
2019-09-23 17:37:15 +0300

    UGOLOC-732-settings: Изменение 'состояния' в моделе блока Удобства и услуги

--- src/components/Settings/services.vue
+++ src/components/Settings/services.vue
@@ -3,32 +3,44 @@
     h6.q-mb-md Удобства и услуги
     .col.q-pl-md
       .row.q-pb-md
-        q-checkbox(v-model="isDressing" label="Отдельная гримерная" dense)
-      .row.q-pb-md
-        q-checkbox(v-model="isWifi" label="Wi-Fi" dense)
-      .row.q-pb-md
-        q-checkbox(v-model="isShower" label="Душ" dense)
-      .row.q-pb-md
-        q-checkbox(v-model="isSnack" label="Снэк-автомат" dense)
-      .row.q-pb-md
-        q-checkbox(v-model="isParty" label="Организация мероприятий" dense)
-      .row.q-pb-md
-        q-checkbox(v-model="isLeasing" label="Прокат оборудования" dense)
-      .row.q-pb-md
-        q-checkbox(v-model="isParking" label="Бесплатная парковка" dense)
+        q-option-group(v-model="group" :options="services" type="checkbox")
 </template>
 
 <script>
 export default {
   name: 'datas',
   data: () => ({
-    isDressing: true,
-    isWifi: true,
-    isShower: true,
-    isSnack: true,
-    isParty: false,
-    isLeasing: true,
-    isParking: false,
+    group: ['isDressing', 'isWifi', 'isShower', 'isSnack', 'isLeasing'],
+    services: [
+      {
+        label: 'Отдельная гримерная',
+        value: 'isDressing'
+      },
+      {
+        label: 'Wi-Fi',
+        value: 'isWifi'
+      },
+      {
+        label: 'Душ',
+        value: 'isShower'
+      },
+      {
+        label: 'Снэк-автомат',
+        value: 'isSnack'
+      },
+      {
+        label: 'Организация мероприятий',
+        value: 'isParty'
+      },
+      {
+        label: 'Прокат оборудования',
+        value: 'isLeasing'
+      },
+      {
+        label: 'Бесплатная парковка',
+        value: 'isParking'
+      }
+    ]
   })
 }
 </script>
