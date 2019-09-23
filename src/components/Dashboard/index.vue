5dda0a444e8421551207f0a3065f7691afd2a789
zarvat <zarvat@localhost.localdomain>
2019-09-23 10:51:46 +0700

    UGOLOC-748 Структура карты бронирования

---
+++ src/components/Dashboard/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  q-card.column.q-py-md
+    profit-chart.q-pa-sm(style="width: 33%;")
+    profit-chart.q-pa-sm(style="width: 33%;")
+    profit-chart.q-pa-sm(style="width: 33%;")
+
+</template>
+
+<script>
+import ProfitChart from './Modules/ProfitChart'
+
+export default {
+  name: 'Dashboard',
+  components: { ProfitChart }
+}
+</script>
+
+<style scoped>
+</style>
