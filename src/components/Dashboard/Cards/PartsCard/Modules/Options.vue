cdc41f8eddda9de1079d4814f665f1fb9971a9e6
zarvat <zarvat@localhost.localdomain>
2019-09-24 16:31:46 +0700

    UGOLOC-749 Компонент графика доля в бронированиях фейк-залы

--- src/components/Dashboard/Cards/PartsCard/Modules/Options.vue
+++ src/components/Dashboard/Cards/PartsCard/Modules/Options.vue
@@ -1,10 +1,56 @@
 <template lang="pug">
   q-card-section
-    q-option-group(
-      v-model="checkedOptions"
-      :options="options"
-      type="checkbox"
-    )
+    q-list
+      q-item
+        q-item-section.text-bold
+          span {{ "Зал" }}
+        q-item-section(side).text-bold
+          span {{ "% р." }}
+      q-item
+        q-item-section(avatar)
+          q-icon(color="blue" name="far fa-circle")
+        q-item-section
+          span {{ "Зал 12" }}
+        q-item-section
+          span {{ 46 }}
+        q-item-section(side)
+          span {{ 4600 }}
+      q-item
+        q-item-section(avatar)
+          q-icon(color="green" name="far fa-circle")
+        q-item-section
+          span {{ "Зал 11" }}
+        q-item-section
+          span {{ 18 }}
+        q-item-section(side)
+          span {{ 1800 }}
+      q-item
+        q-item-section(avatar)
+          q-icon(color="black" name="far fa-circle")
+        q-item-section
+          span {{ "Зал 13" }}
+        q-item-section
+          span {{ 26 }}
+        q-item-section(side)
+          span {{ 2600 }}
+      q-item
+        q-item-section(avatar)
+          q-icon(color="red" name="far fa-circle")
+        q-item-section
+          span {{ "Зал 14" }}
+        q-item-section
+          span {{ 6 }}
+        q-item-section(side)
+          span {{ 600 }}
+      q-item
+        q-item-section(avatar)
+          q-icon(color="orange" name="far fa-circle")
+        q-item-section
+          span {{ "Зал 15" }}
+        q-item-section
+          span {{ 4 }}
+        q-item-section(side)
+          span {{ 400 }}
 
 </template>
 
@@ -17,22 +63,6 @@ export default {
     }
   },
   computed: {
-    options () {
-      let arr = []
-      this.$app.studios.list.forEach(item => {
-        const total = {
-          label: `${item.name} - Предоплата`,
-          value: `${item.name} - Предоплата`
-        }
-        const prepayment = {
-          label: `${item.name} - Бронирования`,
-          value: `${item.name} - Бронирования`
-        }
-        arr.push(total)
-        arr.push(prepayment)
-      })
-      return arr
-    }
   }
 }
 </script>
