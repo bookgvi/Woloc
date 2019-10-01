99cf444e66c0205ee22589e2e1588647e6f43a63
Vitaliy Guselnikov <vitaliy.guselnikov@ucann.ru>
2019-10-01 18:55:37 +0300

    UGOLOC-768: Верстка модального окна Календарь цен

--- src/components/Settings/Room/payment.vue
+++ src/components/Settings/Room/payment.vue
@@ -4,10 +4,18 @@
       .col
         .text-h6.q-pb-md Оплата
         q-btn.bg-primary.text-white(label="Открыть календарь цен" no-caps)
+          q-popup-proxy(v-model="isModal" :offset="[100, 0]")
+            q-card
+              priceCalendar(@hide="isModal = false")
 </template>
 
 <script>
+import priceCalendar from './priceCalendar'
 export default {
-  name: 'payment'
+  name: 'payment',
+  components: { priceCalendar },
+  data: () => ({
+    isModal: false
+  })
 }
 </script>
---
+++ src/components/Settings/Room/priceCalendar.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  .priceCalendar.q-pa-md
+    .row.q-pb-lg
+      .col-9
+        .text-h6 Календарь цен
+      q-space
+      .col
+        q-btn(icon="close" flat @click="$emit('hide')")
+    .row.q-pb-lg
+      .col-6.q-pr-sm.column.justify-end
+        div
+          q-btn(icon="photo_camera")
+          q-btn(icon="videocam")
+          q-btn.q-mr-md(icon="local_play")
+      .col-6
+        span Вместимость зала, человек
+        q-input(v-model="men" outlined dense)
+    .row.q-pb-lg
+      .col.q-pr-sm
+        span Гости без доплаты
+        q-input(v-model="var1" outlined dense)
+      .col.q-pr-sm
+        span Доплата за след, р/час
+        q-input(v-model="var2" outlined dense)
+      .col.q-pr-sm
+        span Скидка "За день"
+        q-input(v-model="var3" outlined dense)
+          template(#append)
+            span(style="font-size: 1rem") %
+      .col
+        span Скидка "День в день"
+        q-input(v-model="var4" outlined dense)
+          template(#append)
+            span(style="font-size: 1rem") %
+    .row.q-pb-lg
+      q-checkbox(label="Наценка на единственный час" v-model="markup")
+</template>
+
+<script>
+export default {
+  name: 'priceCalendar',
+  data: () => ({
+    men: 30,
+    var1: 8,
+    var2: 50,
+    var3: 5,
+    var4: 15,
+    markup: true
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
