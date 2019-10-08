fbd160534fa8a0adaf6fdc7f4865943a9065f1a0
Vitaliy Guselnikov <vitaliy.guselnikov@ucann.ru>
2019-10-08 15:25:59 +0300

    UGOLOC-794: Добавлен переключатель для скрытия части фото

--- src/components/Settings/Location/images.vue
+++ src/components/Settings/Location/images.vue
@@ -2,8 +2,16 @@
   .images
     h6.q-mb-md Изображения
     .row.q-pb-sm
-      q-btn.q-btn--no-uppercase(label="Выбрать файл" outline dense)
-    .row.no-wrap.q-pb-lg
+      .col
+        q-btn.q-btn--no-uppercase(label="Выбрать файл" outline dense)
+      .col-2
+        q-toggle(v-model="isShow" title="Показать все фото")
+    .row.no-wrap.q-pb-lg(v-if="!isShow")
+      draggable(v-model="images")
+        .inline-block(v-for="index in 4" :key="index" title="Drag and drop")
+          q-img.q-mr-sm.q-mb-sm.cursor-pointer(:src="images[index - 1].url" style="height: 140px; width: 140px")
+            q-btn.absolute-top-right(icon="close" class="block" dense flat color="white" title="close")
+    .row.no-wrap.q-pb-lg(v-if="isShow")
       draggable(v-model="images")
         .inline-block(v-for="(item, index) in images" :key="index" title="Drag and drop")
           q-img.q-mr-sm.q-mb-sm.cursor-pointer(:src="item.url" style="height: 140px; width: 140px")
@@ -16,6 +24,7 @@ export default {
   name: 'images',
   components: { draggable },
   data: () => ({
+    isShow: false,
     images: [
       { url: 'http://placeimg.com/640/480/animals' },
       { url: 'http://placeimg.com/640/480/nature' },
