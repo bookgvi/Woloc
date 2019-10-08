8942ddb5749f3c875c6979b393f580d0e8b230f6
Vitaliy Guselnikov <vitaliy.guselnikov@ucann.ru>
2019-10-08 14:43:55 +0300

    UGOLOC-794: Фото в разделе Изображения можно перетаскивать, меняя местами

--- src/components/Settings/Location/images.vue
+++ src/components/Settings/Location/images.vue
@@ -4,29 +4,37 @@
     .row.q-pb-sm
       q-btn.q-btn--no-uppercase(label="Выбрать файл" outline dense)
     .row.no-wrap.q-pb-lg
-      q-img.col.q-mr-md(src="http://placeimg.com/640/480/tech")
-        q-btn.absolute-top-right(icon="close" class="block" dense flat color="white")
-      q-img.col.q-mr-md(src="http://placeimg.com/640/480/animals")
-        q-btn.absolute-top-right(icon="close" class="block" dense flat color="white")
-      q-img.col.q-mr-md(src="http://placeimg.com/640/480/nature")
-        q-btn.absolute-top-right(icon="close" class="block" dense flat color="white")
-      q-img.col(src="http://placeimg.com/640/480/arch")
-        q-btn.absolute-top-right(icon="close" class="block" dense flat color="white")
+      draggable(v-model="images")
+        .inline-block(v-for="(item, index) in images" :key="index" title="Drag and drop")
+          q-img.q-mr-sm.q-mb-sm.cursor-pointer(:src="item.url" style="height: 140px; width: 140px")
+            q-btn.absolute-top-right(icon="close" class="block" dense flat color="white" title="close")
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 export default {
   name: 'images',
+  components: { draggable },
   data: () => ({
-    img1: true,
-    img2: true,
-    img3: true,
-    img4: true,
-    img5: true
+    images: [
+      { url: 'http://placeimg.com/640/480/animals' },
+      { url: 'http://placeimg.com/640/480/nature' },
+      { url: 'http://placeimg.com/640/480/people' },
+      { url: 'http://placeimg.com/640/480/animals' },
+      { url: 'http://placeimg.com/640/480/architecture' },
+      { url: 'http://placeimg.com/640/480/animals' },
+      { url: 'http://placeimg.com/640/480/nature' },
+      { url: 'http://placeimg.com/640/480/people' },
+      { url: 'http://placeimg.com/640/480/animals' },
+      { url: 'http://placeimg.com/640/480/architecture' },
+      { url: 'http://placeimg.com/640/480/tech' }
+    ]
   })
 }
 </script>
 
 <style scoped>
-
+  .closeBtn {
+    position: relative;
+  }
 </style>
