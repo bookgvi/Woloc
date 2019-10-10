948a4ee2767c9e26c917243d2d5a17f7aecd0929
zarvat <zarvat@localhost.localdomain>
2019-10-10 16:26:24 +0700

    UGOLOC-793 карта загруженность

--- src/components/Dashboard/Cards/WorkloadCard/index.vue
+++ src/components/Dashboard/Cards/WorkloadCard/index.vue
@@ -7,65 +7,67 @@
         @dateChange="selectedDate = $event"
         @studioChange="studio = $event"
       )
-      workload-options
+      q-card-section.q-pb-none
+        q-markup-table.q-pb-md(
+          separator="none"
+          dense
+          flat
+        )
+          thead.text-left
+            tr
+              th(style="width: 70%")
+                span.text-bold.text-black.text-body2 Зал
+              th
+                span.text-bold.text-black.text-body2 %
+              th
+                span.text-bold.text-black.text-body2 часы
+          tbody
+            tr(
+              v-for="(load, index) in options"
+              :key="index"
+            )
+              td
+                q-icon.q-mr-md(
+                  :style="{color: load.color}"
+                  name="far fa-circle"
+                )
+                span {{ load.name }}
+              td
+                span.text-grey.text-caption {{ load.percents }}
+              td
+                span.text-body1 {{ load.hours }}
 </template>
 
 <script>
 
 import NavBar from '../CommonModules/NavBar'
-import WorkloadOptions from './Modules/Options'
 
 export default {
   name: 'WorkloadCard',
-  components: { WorkloadOptions, NavBar },
+  components: { NavBar },
   data () {
     return {
       selectedDate: this.$moment({ hour: 0 }).parseZone(),
       studio: (this.$app.studios.list.length > 0) ? this.$app.studios.list[0].id : 0,
-      isMiniTable: true
     }
   },
   computed: {
-    bookings () {
-      return (this.isMiniTable) ? this.$app.bookings.dashboardList.slice(0, 3) : this.$app.bookings.dashboardList
+    rooms () {
+      return this.$app.rooms.getAvailable({ studio: this.studio })
+    },
+    options () {
+      if (!this.rooms) return []
+      return this.rooms.map(item => {
+        return {
+          name: item.name,
+          color: '#' + ((1 << 24) * Math.random() | 0).toString(16),
+          hours: item.name.length,
+          percents: item.name.length * 5
+        }
+      })
     }
   },
   methods: {
-    async loadData () {
-      const filter = {
-        studio: this.studio,
-      }
-      await this.$app.bookings.getForDashBoard({
-        ...filter,
-        dateFrom: this.selectedDate.format('YYYY-MM-DD'),
-        dateTo: this.$moment(this.selectedDate).add(1, 'days').format('YYYY-MM-DD')
-      })
-    },
-    clientSlot (index) {
-      const booking = this.bookings[index]
-      return `${booking.customer.fullName} • ${booking.duration} ч.`
-    },
-    prepaymentSlot (index) {
-      const booking = this.bookings[index]
-      return (booking.amount).toLocaleString('ru-RU', { style: 'decimal', useGrouping: true })
-    },
-    paymentSlot (index) {
-      const booking = this.bookings[index]
-      return (booking.price).toLocaleString('ru-RU', { style: 'decimal', useGrouping: true })
-    },
-  },
-  watch: {
-    async selectedDate () {
-      await this.loadData()
-    },
-    studio: {
-      async handler () {
-        if (this.studio !== 0) {
-          await this.loadData()
-        }
-      },
-      immediate: true
-    },
   }
 }
 </script>
