38718cbd01c04f7a3e7d1498e3f5e3865900f461
zarvat <zarvat@localhost.localdomain>
2019-10-16 23:49:10 +0700

    UGOLOC-779 отображение комментария к технической броне если есть

--- src/components/Calendar/Sheet/index.vue
+++ src/components/Calendar/Sheet/index.vue
@@ -98,6 +98,8 @@
               .row.col-12(v-if="!e.technical")
                 span.row.col-12.text-booking.wrap {{ e.title }}
                 span.row.col-12.text-booking.wrap {{ e.details }}
+              .row.col-12(v-else)
+                span.row.col-12.text-booking.wrap {{ e.managerComment }}
       update-event-dialog(
         :dialogState="dialogState"
         :filter="filter"
