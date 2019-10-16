bf4d39b5fdedbce41f77b809b0baa903c10e4c4b
zarvat <zarvat@localhost.localdomain>
2019-10-16 14:43:31 +0700

    UGOLOC-807 исправление заголовка при невыбранной студии

--- src/components/Calendar/Sheet/index.vue
+++ src/components/Calendar/Sheet/index.vue
@@ -149,7 +149,7 @@ export default {
       return this.isAllDay ? 24 : 16
     },
     selectedStudioLabel () {
-      return this.studio ? this.studio.name : ''
+      return this.studio ? this.studio.name : 'Студия не выбрана'
     },
     studio () {
       return this.$app.studios.getFiltered(this.filter)
