f5bb97525cc6b1ab0e69d7306f285131c486c391
Vitaliy Guselnikov <vitaliy.guselnikov@ucann.ru>
2019-10-16 15:29:02 +0300

    UGOLOC-801: Блок вставки для виджета

--- src/components/Settings/JSWidget/index.vue
+++ src/components/Settings/JSWidget/index.vue
@@ -7,17 +7,17 @@
         .row.q-pb-xs
           .col.q-pr-sm
             span Локация
-          .col.q-pr-sm
+          .col
             span Залы
         .row.q-pb-md
           .col.q-pr-sm
             q-select(v-model="currentStudio" :options="data.studios" outlined dense)
           .col
-            q-select(v-model="currentRoom" :options="data.rooms" outlined dense)
+            q-select(v-model="currentRoom" :options="data.rooms.map(item => item.name)" outlined dense)
         .row.q-pb-xs
           .col.q-pr-sm
             span Фон
-          .col.q-pr-sm
+          .col
             span Источник бронирования
         .row.q-pb-md
           .col.q-pr-sm
@@ -40,6 +40,15 @@
               style="width: 100%;"
               no-caps
             )
+        .row.q-pb-xs.justify-around
+          .col.q-pr-sm
+            span Код для вставки
+          .col.q-pr-sm
+            .text-primary.cursor-pointer(@click="copyWidget" style="text-align: right;") Скопировать
+        .row.q-pb-md
+          .col
+            q-input.jswidgetArea(v-model="widgetCode" type="textarea" rows=30 outlined dense)
+
 </template>
 
 <script>
@@ -51,18 +60,57 @@ export default {
     currentBackground: '',
     currentSource: '',
     currentUnit: '',
+    widgetCode: `<!-- BEGIN UGOLOC CODE {literal} -->
+    <div id='ugoloc'></div>
+    <script type='text/javascript'>
+    var widgetUrl = 'http://ugoloc.greencow.pro';
+    (function () {
+    var room_id = 41;
+    var timestamp = Math.round(Date.now()/10000);
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', widgetUrl + '/embed/booking.json?id=' + room_id);
+    xhr.onload = function() {
+    if (xhr.status === 200) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = widgetUrl + '/assets/embed/ugoloc.embed.js?t=' + timestamp;
+    var styles = document.createElement('link');
+    styles.type = 'text/css';
+    styles.rel = 'stylesheet';
+    styles.href = widgetUrl + '/assets/embed/ugoloc.css?t=' + timestamp;
+    var ugoloc = document.getElementById('ugoloc');
+    var json = JSON.parse(xhr.responseText);
+    document.body.appendChild(styles);
+    ugoloc.innerHTML = json.html;
+    ugoloc.appendChild(script);
+    }
+    };
+    xhr.send();
+    })();
+    <script>
+    <!-- {/literal} END UGOLOC CODE -->`,
     data: {}
   }),
   async created () {
     this.data = await this.$app.jswidget.getAll()
     this.currentStudio = this.data.studios[0]
-    this.currentRoom = this.data.rooms[0]
+    this.currentRoom = this.data.rooms[0].name
     this.currentBackground = this.data.background[0]
     this.currentUnit = this.data.widthUnit[0]
   },
   methods: {
     generate () {
       this.currentSource = ''
+    },
+    copyWidget () {
+      const widgetNode = document.querySelector('.jswidgetArea textarea')
+      widgetNode.focus()
+      widgetNode.select()
+      try {
+        document.execCommand('copy')
+      } catch (err) {
+        console.error(`Can't copy`)
+      }
     }
   }
 }
