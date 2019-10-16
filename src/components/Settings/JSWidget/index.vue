7cdf8ee080e45f6bbfd11d617481c1b5dfe73f6e
Vitaliy Guselnikov <vitaliy.guselnikov@ucann.ru>
2019-10-16 12:57:43 +0300

    UGOLOC-801: маршрутизация до страницы

---
+++ src/components/Settings/JSWidget/index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .jswidget
+    .row.q-pb-md
+      .text-h5 JS Widget
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
