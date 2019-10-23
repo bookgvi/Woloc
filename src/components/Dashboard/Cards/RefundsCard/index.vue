0d912778ae6b23634791a620ecad5391e0966ae3
zarvat <zarvat@localhost.localdomain>
2019-10-23 18:04:37 +0700

    UGOLOC-793 подключен метод по возвратам

--- src/components/Dashboard/Cards/RefundsCard/index.vue
+++ src/components/Dashboard/Cards/RefundsCard/index.vue
@@ -20,22 +20,22 @@
             :key="index"
           )
             td
-              q-checkbox(v-model="item.check" dense)
+              q-checkbox(v-model="checkedOptions[index]" dense)
               span &nbsp {{ item.name }}
             td.text-right
               q-icon(
-                v-if="item.isInfo"
+                v-if="item.status.id !== 0"
                 size="7px"
                 color="red"
                 name="fas fa-circle"
                 style="cursor: pointer"
               )
                 q-tooltip
-                  span.row.text-body2 28 октября 9:00-13:00
+                  span.row.text-body2 {{ firstDateSlot(item.createdAt) }}
                   span.row &nbsp
-                  span.row.text-body2.text-red Возврат 28 октября 12:00
+                  span.row.text-body2.text-red {{ firstDateSlot(item.returnedAt) }}
             td.text-right
-              span.text-black.text-body2 {{ item.value }} р.
+              span.text-black.text-body2 {{ item.value.toLocaleString('ru-RU', { style: 'decimal', useGrouping: true }) }} р.
     q-card-actions.q-pl-md.q-pb-md
       q-btn.q-mr-xs.col-3.text-body2.text-black(
         outline
@@ -66,33 +66,51 @@ export default {
   components: { StandartCard, NameSlot },
   data () {
     return {
-      options: [
-        {
-          name: 'Антон Куранов',
-          check: false,
-          isInfo: true,
-          value: 1600
-        },
-        {
-          name: 'Роман Марков',
-          check: false,
-          isInfo: false,
-          value: 2400
-        }
-      ]
+      checkedOptions: []
     }
+  },
+  async created () {
+    await this.loadData()
   },
   computed: {
     enabled () {
-      return this.options.some(item => item.check)
-    }
+      return this.checkedOptions.some(item => item)
+    },
+    options () {
+      const refundsList = this.$app.refunds.dashboardRefundsList.map(item => {
+        return {
+          name: item.booking.customer.fullName,
+          check: false,
+          value: item.amount,
+          status: item.status,
+          createdAt: item.createdAt,
+          returnedAt: item.returnedAt
+        }
+      })
+      console.log(this.$app.refunds.dashboardRefundsList, refundsList)
+      return refundsList
+    },
   },
   methods: {
+    firstDateSlot (dt) {
+      return this.$moment(dt).format('DD MMMM hh:mm')
+    },
+    async loadData () {
+      await this.$app.refunds.getForDashBoard()
+    },
     accept () {
       //
     },
     decline () {
       //
+    }
+  },
+  watch: {
+    options: {
+      handler (v) {
+        this.checkedOptions = Array(v.length).fill(false)
+      },
+      immediate: true
     }
   }
 }
