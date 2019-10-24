61e344954dacec551d2fd8fc80d47fab286b28cb
Vitaliy Guselnikov <vitaliy.guselnikov@ucann.ru>
2019-10-24 14:27:54 +0300

    UGOLOC-789: удалена ненужная проверка для фильтров

--- src/components/DataTable/connectedMixin.js
+++ src/components/DataTable/connectedMixin.js
@@ -10,33 +10,7 @@ export default {
   },
   methods: {
     async onRequest (pagination, filter) {
-      console.log('connectedMixin', filter)
       const { page, rowsPerPage } = pagination
-      let isEmpty = 0
-      if (this.$route.path === '/bookings') {
-        if ((filter.studio && !filter.rooms) || (filter.studio && filter.rooms.length)) {
-          isEmpty = false
-          this.fetchData(page, rowsPerPage, filter, pagination, isEmpty)
-        } else {
-          isEmpty = true
-          this.fetchData(page, rowsPerPage, filter, pagination, isEmpty)
-        }
-      } else {
-        isEmpty = false
-        this.fetchData(page, rowsPerPage, filter, pagination, isEmpty)
-      }
-    },
-    setPagination (prop, value) {
-      const { pagination, filter } = this
-      if (pagination[prop] === value) return
-
-      this.onRequest({ ...pagination, [prop]: value }, filter)
-    },
-    async fetchData (page, rowsPerPage, filter, pagination, isEmpty) {
-      if (isEmpty) {
-        this.data = []
-        return
-      }
       let { items, total, data } = await this.loadData({ number: page, size: rowsPerPage }, filter)
       if (data) {
         this.account.amount = data.account.amount
@@ -45,6 +19,12 @@ export default {
       }
       this.data = items
       Object.assign(this.pagination, pagination, { rowsNumber: total })
+    },
+    setPagination (prop, value) {
+      const { pagination, filter } = this
+      if (pagination[prop] === value) return
+
+      this.onRequest({ ...pagination, [prop]: value }, filter)
     }
   },
   mounted () {
