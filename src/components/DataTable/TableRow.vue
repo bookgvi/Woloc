8885eb83c07ad6810d6d400056b284eee41190a5
Vitaliy Guselnikov <vitaliy.guselnikov@ucann.ru>
2019-10-28 10:49:04 +0300

    UGOLOC-887: проверка параметра opacity

--- src/components/DataTable/TableRow.vue
+++ src/components/DataTable/TableRow.vue
@@ -122,7 +122,7 @@ export default {
       const r = parseInt(hex.substring(0, 2), 16)
       const g = parseInt(hex.substring(2, 4), 16)
       const b = parseInt(hex.substring(4, 6), 16)
-      const result = `rgba(${r}, ${g}, ${b}, ${opacity / 100})`
+      const result = `rgba(${r}, ${g}, ${b}, ${opacity > 1 ? opacity / 100 : opacity})`
       return result
     }
   }
