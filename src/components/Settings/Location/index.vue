da5bb8a1a7ebe312bf1074db7b667e94a2eed880
Vitaliy Guselnikov <vitaliy.guselnikov@ucann.ru>
2019-11-05 14:15:40 +0300

    UGOLOC-903: дефолтные значения для фильтров на странице /settings

--- src/components/Settings/Location/index.vue
+++ src/components/Settings/Location/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .settings
+  .settings(:key="pageReload")
     div(v-show="false") {{ studioID }}
     q-tab-panels(v-model="currentTab")
       q-tab-panel.q-pa-none(name="Локация")
@@ -35,6 +35,7 @@ export default {
   },
   data () {
     return {
+      pageReload: 0,
       id: this.$app.filters.getValues('settings').studio,
       isRequiredModal: false,
       currentTab: 'Локация',
@@ -56,16 +57,23 @@ export default {
   },
   methods: {
     async singleStudioM () {
-      let { studio } = this.$app.filters.getValues('settings')
+      let filter = {}
       const { items } = await studios.getAll().then(resp => resp.data)
+      let { studio } = this.$app.filters.getValues('settings')
+      const rooms = items[0].rooms
       if (!studio) {
+        const roomsID = rooms.map(item => item.id)
+        filter = Object.assign({}, {
+          studio: items[0].id,
+          rooms: roomsID
+        })
+        this.$app.filters.setValue('settings', 'studio', filter.studio)
+        this.$app.filters.setValue('settings', 'rooms', filter.rooms)
         studio = items[0].id
       }
-      this.currentStudio = studio
-      this.isSave = false
-      const [{ rooms }] = items.filter(item => item.id === studio)
       this.rooms = rooms
       this.singleStudio = await studios.getOne(studio).then(resp => resp.data)
+      this.allStudiosName = items.map(item => item.name)
       this.services = this.singleStudio.services
       this.vendors = this.singleStudio.vendors
     },
@@ -112,6 +120,7 @@ export default {
       if (result) {
         this.isSave = false
       }
+      this.pageReload++
     }
   },
   async mounted () {
