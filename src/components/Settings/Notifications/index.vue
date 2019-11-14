18119a7b41cc34d041bcfa4fff66f69acfe5adb1
zarvat <zarvat@192.168.0.4>
2019-11-14 15:00:42 +0700

    UGOLOC-928 заготовка страницы

---
+++ src/components/Settings/Notifications/index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  .notifications
+    .row.justify-center
+      h1 E-mail уведомления
+      span.text-bold Получать информацию:
+      q-option-group(
+        type="checkbox"
+        :options="options"
+        v-model="checkedOptions"
+      )
+</template>
+
+<script>
+export default {
+  name: 'notifications',
+  data () {
+    return {
+      checkedOptions: [],
+      options: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
