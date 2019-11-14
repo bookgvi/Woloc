0fb4cc143d99818221a1c7f689f7d7f91b1ae5c8
zarvat <zarvat@192.168.0.4>
2019-11-14 15:43:39 +0700

    UGOLOC-928 страница

--- src/components/Settings/Notifications/index.vue
+++ src/components/Settings/Notifications/index.vue
@@ -1,13 +1,14 @@
 <template lang="pug">
   .notifications
-    .row.justify-center
-      h1 E-mail уведомления
-      span.text-bold Получать информацию:
-      q-option-group(
-        type="checkbox"
-        :options="options"
-        v-model="checkedOptions"
-      )
+    .row.q-py-lg.justify-center
+      .col-4
+        .text-h6.q-pb-md.q-pl-sm E-mail уведомления
+        span.q-pl-sm.text-bold Получать информацию:
+        q-option-group.q-pt-md.q-px-none(
+          type="checkbox"
+          :options="options"
+          v-model="checkedOptions"
+        )
 </template>
 
 <script>
@@ -16,7 +17,28 @@ export default {
   data () {
     return {
       checkedOptions: [],
-      options: []
+      options: [
+        {
+          label: 'Об отмене бронирования',
+          value: 0
+        },
+        {
+          label: 'О новых бронированиях',
+          value: 1
+        },
+        {
+          label: 'О новых заявках',
+          value: 2
+        },
+        {
+          label: 'О возврате предоплаты',
+          value: 3
+        },
+        {
+          label: 'О новых сообщениях',
+          value: 4
+        },
+      ]
     }
   }
 }
