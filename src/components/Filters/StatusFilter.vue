c52a72bf830a363346648eab697dfee97a02428c
book <bookgvi@gmail.com>
2019-11-19 22:09:45 +0300

    merge

--- src/components/Filters/StatusFilter.vue
+++ src/components/Filters/StatusFilter.vue
@@ -10,7 +10,7 @@
 
 <script>
 import FilterSelect from './FilterSelect'
-import { BOOKING_STATUSES } from 'src/common/constants'
+import { BOOKING_STATUSES, REFUNDS_STATUSES } from 'src/common/constants'
 
 export default {
   name: 'status-filter',
@@ -22,8 +22,15 @@ export default {
     onChange: Function
   },
   data: () => ({
-    options: Object.values(BOOKING_STATUSES)
+    options: []
   }),
+  mounted () {
+    if (this.$route.path === '/bookings') {
+      this.options = Object.values(BOOKING_STATUSES)
+    } else {
+      this.options = Object.values(REFUNDS_STATUSES)
+    }
+  },
   computed: {
     value () {
       return this.values.statuses || []
