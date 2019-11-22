14743847c926987f2c8bac951f691c3b70396378
Виатлий <vitaliy.guselnikov@ucann.ru>
2019-11-22 19:34:56 +0300

    UGOLOC-985: корректировка условия в фильтре

--- src/components/Filters/FiltersList.vue
+++ src/components/Filters/FiltersList.vue
@@ -29,7 +29,15 @@ export default {
   computed: {
     values () {
       const filter = this.$app.filters.readFromSession()[this.name]
-      if (filter.hasOwnProperty('studio') && !filter.studio && filter.hasOwnProperty('customer') && !filter.customer) {
+      if (this.name === 'bookings' &&
+        filter.hasOwnProperty('studio') &&
+        !filter.studio &&
+        filter.hasOwnProperty('customer') &&
+        !filter.customer) {
+        this.$app.filters.reset(this.name)
+      } else if (this.name !== 'bookings' &&
+        filter.hasOwnProperty('studio') &&
+        !filter.studio) {
         this.$app.filters.reset(this.name)
       }
       return this.$app.filters.getValues(this.name)
