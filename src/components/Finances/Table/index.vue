8e5308447141cfc2192856d45cbb19eef970d67e
Виатлий <vitaliy.guselnikov@ucann.ru>
2019-11-25 16:24:03 +0300

    UGOLOC-751: Стилизация выпадающего меню /finances

--- src/components/Finances/Table/index.vue
+++ src/components/Finances/Table/index.vue
@@ -11,29 +11,30 @@
       template(#title-append="props")
         .text-h6.inline-block.q-pl-md {{ props.amount }} ₽
       template(#table-controls-prepend="props")
-        q-btn.bg-primary.q-mr-xs(no-caps text-color='white' label="Вывести на счет")
+        q-btn.bg-primary(no-caps text-color='white' label="Вывести на счет")
         q-btn.bg-primary.text-white.q-mr-md.q-pa-none
           q-icon(name="expand_more")
-          q-popup-proxy.q-mt-md
-            q-card
-              .row
-                .col
-                  q-btn.q-py-none.q-px-sm.text-bold.text-subtitle1.text-primary(
-                    outline
-                    label="Отметить предоплату"
-                    @click="isPrepayment = true"
-                    v-close-popup
-                    style="width: 100%;"
-                  )
-              .row
-                .col
-                  q-btn.q-py-none.q-px-sm.text-bold.text-subtitle1.text-primary(
-                    outline
-                    label="Сделать возврат"
-                    @click="isRefund = true"
-                    v-close-popup
-                    style="width: 100%;"
-                  )
+        q-popup-proxy.q-mt-md
+          q-card(style="width: 180px;")
+            .row
+              .col
+                q-btn.q-py-none.q-px-sm(
+                  no-caps
+                  flat
+                  label="Отметить предоплату"
+                  @click="isPrepayment = true"
+                  v-close-popup
+                  style="width: 100%;"
+                )
+            .row
+              .col
+                q-btn.q-py-none.q-px-sm(
+                  no-caps
+                  label="Сделать возврат"
+                  @click="isRefund = true"
+                  v-close-popup
+                  style="width: 100%;"
+                )
     q-dialog(v-model="isPrepayment")
       q-card(style="min-width: 640px;")
         prepayment
