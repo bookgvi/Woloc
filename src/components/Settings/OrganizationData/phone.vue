3641e884a3e18476ed1f295e404ad0d7c63cb65a
Виатлий <vitaliy.guselnikov@ucann.ru>
2019-12-04 13:51:32 +0300

    UGOLOC-1019: смена названия для адреса, расположение элементов для блока Телефо

--- src/components/Settings/OrganizationData/phone.vue
+++ src/components/Settings/OrganizationData/phone.vue
@@ -1,22 +1,22 @@
 <template lang="pug">
   .phoneComponent
-    .row.q-pb-sm(v-for="(item, index) in arrayLength" :key="index")
-      .col
-        q-input(
-          ref="tel"
-          v-model="organization.phones[index]"
-          type="tel"
-          mask=" +# (###) ### - ####"
-          unmasked-value
-          hint="Mask: +# (###) ### - ####"
-          :rules="[val => val.length === 11 || 'Номер должен содержать 11 цифр']"
-          outlined
-          dense
-        )
-          template(#after)
-            q-icon.cursor-pointer(name="delete" @click="deletePhone(index)" title="Удалить телефон")
-    .phoneBtn.col
-      q-btn.q-mt-sm.bg-primary.text-white(label="Добавить телефон" @click="addPhone" outlined style="width: 100%;")
+      .row.q-pb-sm(v-for="(item, index) in arrayLength" :key="index")
+        .col
+          q-input(
+            ref="tel"
+            v-model="organization.phones[index]"
+            type="tel"
+            mask=" +# (###) ### - ####"
+            unmasked-value
+            hint="Mask: +# (###) ### - ####"
+            :rules="[val => val.length === 11 || 'Номер должен содержать 11 цифр']"
+            outlined
+            dense
+          )
+            //template(#after)
+              q-icon.cursor-pointer(name="delete" @click="deletePhone(index)" title="Удалить телефон")
+        .phoneBtn.col.q-pl-sm
+          q-btn.q-mb-sm(v-show="index === arrayLength - 1" label="Добавить телефон" @click="addPhone" no-caps style="width: 100%;")
 </template>
 
 <script>
