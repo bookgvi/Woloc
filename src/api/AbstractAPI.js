f571a5c3bf43031d43ca471dd549ad0572bb5c9e
book <bookgvi@gmail.com>
2019-12-28 11:17:28 +0300

    AbstractAPI - изменен синтаксис, после изменения синатксиса объекта восстановлен конструктор с помощью Object.defineProperty

--- src/api/AbstractAPI.js
+++ src/api/AbstractAPI.js
@@ -4,65 +4,75 @@ export function AbstractAPI (url) {
   this.url = url
 }
 
-AbstractAPI.prototype.getAll = async function (page, filter) {
-  try {
-    const r = await api.get(this.url, {
-      params: {
-        page,
-        ...filter
-      }
-    })
-    return r.data
-  } catch (e) {
-    console.warn('catch :: getAll', e)
-    return e
-  }
-}
-AbstractAPI.prototype.getOne = async function ({ id }) {
-  try {
-    const r = await api.get(`${this.url}/${id}`)
-    return r.data
-  } catch (e) {
-    console.warn('catch :: getOne', e)
-    return e
-  }
-}
-AbstractAPI.prototype.updateOne = async function (payload) {
-  const { id, data } = payload
-  try {
-    const status = await api.put(`${this.url}/${id}`, data)
-    return status.data
-  } catch (err) {
-    console.warn('catch :: UpdateOne', err)
-    return err
-  }
-}
-AbstractAPI.prototype.addNew = async function (payload) {
-  try {
-    const status = await api.post(this.url, payload)
-    return status.data
-  } catch (err) {
-    console.warn('catch :: createNew', err)
-    return err
+AbstractAPI.prototype = {
+  getAll: async function (page, filter) {
+    try {
+      const r = await api.get(this.url, {
+        params: {
+          page,
+          ...filter
+        }
+      })
+      return r.data
+    } catch (e) {
+      console.warn('catch :: getAll', e)
+      return e
+    }
+  },
+
+  getOne: async function ({ id }) {
+    try {
+      const r = await api.get(`${this.url}/${id}`)
+      return r.data
+    } catch (e) {
+      console.warn('catch :: getOne', e)
+      return e
+    }
+  },
+
+  updateOne: async function (payload) {
+    const { id, data } = payload
+    try {
+      const status = await api.put(`${this.url}/${id}`, data)
+      return status.data
+    } catch (err) {
+      console.warn('catch :: UpdateOne', err)
+      return err
+    }
+  },
+
+  addNew: async function (payload) {
+    try {
+      const status = await api.post(this.url, payload)
+      return status.data
+    } catch (err) {
+      console.warn('catch :: createNew', err)
+      return err
+    }
+  },
+
+  deleteOne: async function (payload) {
+    const { id } = payload
+    try {
+      const r = await api.delete(`${this.url}/${id}`)
+      return r.data
+    } catch (e) {
+      console.warn('catch :: deleteOne', e)
+    }
+  },
+
+  uploadImage: async function (payload, config) {
+    try {
+      const status = await api.post(`${this.url}/upload-image`, payload, config)
+      return status.data
+    } catch (err) {
+      console.warn('catch :: uploadImage', err)
+      return err
+    }
   }
 }
 
-AbstractAPI.prototype.deleteOne = async function (payload) {
-  const { id } = payload
-  try {
-    const r = await api.delete(`${this.url}/${id}`)
-    return r.data
-  } catch (e) {
-    console.warn('catch :: deleteOne', e)
-  }
-}
-
-AbstractAPI.prototype.uploadImage = async function (payload, config) {
-  try {
-    const status = await api.post(`${this.url}/upload-image`, payload, config)
-    return status.data
-  } catch (err) {
-    console.warn('catch :: uploadImage', err)
-    return err
-  }
-}
+Object.defineProperty('Abstract.prototype', 'constructor', {
+  value: 'AbstractAPI',
+  enumerable: false
+})
