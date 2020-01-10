1f7a62732fbd7dc9c751d967d86c7abe09fee7b0
book <bookgvi@gmail.com>
2020-01-10 23:24:03 +0300

    Убрана резиновая верстка окна ввода логина/пароля на странице /login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   q-page.bg-blue-grey-2.flex.justify-center.items-center.row
-    form.col-xs-12.col-sm-8.col-md-6.col-lg-4.col-xl-2(@submit.prevent="doLogin")
+    form.col-4(@submit.prevent="doLogin")
       q-card.bg-white
         q-card-section.font-size-large {{ "Логин" }}
         q-card-section
